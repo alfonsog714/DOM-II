2ae6b6cadafca23aae42970905782afe2f1ddf9e
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-09 12:47:28 -0700

    Added a wheel event for the Blog li element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const headerImg = document.querySelector(".intro img");
+const aboutUS = document.querySelector(
+  "header.main-navigation nav a:nth-child(2)"
+);
+const blog = document.querySelector(
+  "header.main-navigation nav a:nth-child(3)"
+);
+
+headerImg.addEventListener("mouseover", event => {
+  event.target.style.padding = "1%";
+  setTimeout(function() {
+    event.target.style.padding = "";
+  }, 500);
+});
+
+blog.addEventListener("wheel", event => {
+  console.log(`Page Y: ${event.pageY}`);
+  console.log(`Page X: ${event.pageX}`);
+  if (event.pageY > 340) {
+    event.currentTarget.style.background = "red";
+  } else {
+    event.currentTarget.style.background = "";
+  }
+});
+
+// aboutUS.addEventListener("keydown", event => {
+//   event.target.setAttribute("href", "https://wwww.google.com");
+// }); extremely delayed
